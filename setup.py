c799a6711043b304c420c1df23b64bfba7312cec
Sidney Mau <sidneymau@gmail.com>
2023-08-24 14:05:57 -0700

    init git repo

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup, find_packages
+
+scripts = [
+    "bin/jupyterm",
+]
+
+setup(
+    name="jupyterm",
+    version=0.1,
+    description="TUI Jupyter notebook viewer",
+    author="smau",
+    packages=find_packages(),
+    scripts=scripts,
+)
